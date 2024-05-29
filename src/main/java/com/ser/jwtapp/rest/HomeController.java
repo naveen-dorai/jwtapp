cc668ab0d89bbd4eb0a614fe8a91ddf7b65dc506
naveen-dorai <naveen.dmswamy@gmail.com>
2024-05-29 16:09:40 +0530

    initial commit

---
+++ src/main/java/com/ser/jwtapp/rest/HomeController.java
@@ -0,0 +1,12 @@
+package com.ser.jwtapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/home")
+    public String home(){
+        return "This is home page";
+    }
+}
